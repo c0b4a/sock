40d99d58c6d4c99596234b64760baa6754531f5e
c0b4a <cobra.inbox@gmail.com>
2023-08-11 21:59:17 -0700

    time packets + sustained connection

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import socket
+import time
+
+#10 digits of bytes max
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -8,5 +12,12 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f'Connection from {address} has been established!')
-    clientsocket.send(bytes('Welcome to the server!', 'utf-8'))
-    clientsocket.close()
+    msg = 'Welcome to the server!'
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    clientsocket.send(bytes(msg, 'utf-8'))
+    
+    while True:
+        time.sleep(3)
+        msg = f'The time is: {time.time()}'
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientsocket.send(bytes(msg, 'utf-8'))
